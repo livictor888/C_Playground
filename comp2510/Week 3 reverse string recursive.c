827a43a70a987ec93c4d3eeedb5ab7c32b4be219
livictor888 <vli30@my.bcit.ca>
2021-09-27 14:57:10 -0700

    Week 3

--- comp2510/Week 3 reverse string recursive.c
+++ comp2510/Week 3 reverse string recursive.c
@@ -3,20 +3,25 @@
 #include <string.h>
 
 
-void reverse(char *inputString, char *reverseString) {
+void reverseWithNoNullTerminotor(char *inputString, char *reverseString) {
     size_t length = strlen(inputString);
     if (!length) {
         return;
     }
 
     reverseString[length -1] = *inputString;
-    reverse(inputString + 1, reverseString);
+    reverseWithNoNullTerminotor(inputString + 1, reverseString);
+}
+
+void reverse(char *inputString, char *reverseString) {
+    reverseWithNoNullTerminotor(inputString, reverseString);
+    reverseString[strlen(inputString)] =0;                      // adds the null terminator
 }
 
 int main() {
     char input[] = "Seyed";
     char output[20] = "Javididfdf";
-    reverse(input, output);
+    reverseWithNoNullTerminotor(input, output);
     printf("Reverse = %s\n", output);
     return 0;
 }
