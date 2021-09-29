fc5f8c02eaebd4906659165949331deac548d111
Victor Li <vli30@my.bcit.ca>
2021-09-28 20:47:19 -0700

    Week 4

--- comp2510/Assignment_1.c
+++ comp2510/Assignment_1.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+#define MAX_WORD_LENGTH 20
+
+#define MAX_NUMBER_OF_WORDS 100
+
+char *moveToBeginningOfNextToken(char *inputString);
+int getCurrentTokenSize(char *inputString);
+int tokenize(char *paragraph, char tokens[MAX_NUMBER_OF_WORDS][MAX_WORD_LENGTH]);
+int getNumberOfWordsForNextLine(char tokens[MAX_NUMBER_OF_WORDS][MAX_WORD_LENGTH],
+                                int numberOfWordsProcessedSoFar,
+                                int totalNumberOfWords,
+                                int lineLength);
+
+
+
 int main(){
     return 0;
 }
--- comp2510/Lab_Week_4.c
+++ comp2510/Lab_Week_4.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
+#include <string.h>
+
+void findMinDistance(char *inputString) {
+    size_t length = strlen(inputString);
+    int characterDistanceCounter;
+    int minDistance = (int) length;
+    char minChar;
+
+    for (int charIndex = 0; charIndex < length -1 ; charIndex++) {
+        characterDistanceCounter = 0;
+        for (int compareIndex = charIndex + 1; compareIndex < length; compareIndex++) {
+            if (inputString[charIndex] == inputString[compareIndex]) {
+                if (characterDistanceCounter < minDistance) {
+                    minDistance = characterDistanceCounter;
+                    minChar = inputString[charIndex];
+                }
+            } else {
+                characterDistanceCounter++;
+            }
+        }
+    }
+    if (minDistance == length) {
+        printf("There is no min distance.\n");
+        return;
+    } else {
+        printf("Repeated char: %c\nThe min distance is: %d\n", minChar, minDistance);
+    }
+}
 
 int main() {
+    char *testString = "Hello there, how's it going?";
+    char *testString2 = "Hi there, how's it going?";
+    char *testString3 = "123";
+
+    findMinDistance(testString);
+    findMinDistance(testString2);
+    findMinDistance(testString3);
     return 0;
 }
