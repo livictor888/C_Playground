5a54bc91facb34812b34b6262fc1dac151adfa36
Victor Li <vli30@my.bcit.ca>
2021-10-05 23:31:25 -0700

    Week 5 Lab

--- comp2510/Lab_Week_5.c
+++ comp2510/Lab_Week_5.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
-#include <string.h>
 
+#define MAX_COLUMNS 10
 
+int fromBinary(int *binaryArray, int size) {
+    if (size == 0) {
+        return 0;
+    }
+    int number = fromBinary(binaryArray, size - 1);
+    return 2 * number + binaryArray[size - 1];
+}
+
+void readNextNumberFromFile(FILE *file, int binaryArray[MAX_COLUMNS], int numberOfColumns) {
+    for (int currentNumber = 0; currentNumber < numberOfColumns; currentNumber ++) {
+        fscanf(file, "%d", &binaryArray[currentNumber]);
+    }
+    printf("%d\n", fromBinary(binaryArray, numberOfColumns));
+}
+
+void processFile(char *fileName) {
+    FILE *file = fopen(fileName, "r");
+    int numberOfRows;
+    int numberOfColumns;
+    fscanf(file, "%d %d", &numberOfRows, &numberOfColumns);
+    int binaryArray[MAX_COLUMNS];
+    for(int row = 0 ;row < numberOfRows; row++) {
+        readNextNumberFromFile(file, binaryArray, numberOfColumns);
+    }
+    fclose(file);
+}
 
 int main() {
-
+    char *fileName = "text.txt";
+    processFile(fileName);
     return 0;
 }
