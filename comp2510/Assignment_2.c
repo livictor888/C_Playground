c0d2b161cd1580541228c836ad60a1aa7ad34cdf
livictor888 <vli30@my.bcit.ca>
2021-10-11 23:38:47 -0700

    A2

--- comp2510/Assignment_2.c
+++ comp2510/Assignment_2.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+#define ERROR_FILE_NOT_FOUND 1
+#define ERROR_TOO_MANY_ARGUMENTS 2
+#define ERROR_MISSING_ARGUMENTS 3
+
+void matchSearchToken(FILE *file, char *searchToken) {
+    int lineNumber;
+    char currentChar;
+    fscanf(file, "%c", currentChar);
+    if (currentChar == *searchToken) {
+        printf("character matches\n");
+    }
+
+}
+
+int processFile(char *fileName, char *searchToken) {
+    FILE *file = fopen(fileName, "r");
+    if (file == NULL) {
+        printf("File not found.\n");
+        return ERROR_FILE_NOT_FOUND;
+    }
+    matchSearchToken(file, searchToken);
+    fclose(file);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    // Arguments: input.txt assignment
+    // Output:    Line: 1, character; 53
+    // Arguments: input.txt third
+    // Output:    Not found!
+    printf("Program name is %s\n", argv[0]);
+    if (argc == 3) {
+        printf("The arguments supplied are %s, %s\n", argv[1], argv[2]);
+        char *fileName = argv[1];
+        char *searchToken = argv[2];
+        processFile(fileName, searchToken);
+    } else if (argc > 3) {
+        printf("Too many arguments supplied.\n");
+        return ERROR_TOO_MANY_ARGUMENTS;
+    } else {
+        printf("Missing arguments.\n");
+        return ERROR_MISSING_ARGUMENTS;
+    }
     return 0;
 }
