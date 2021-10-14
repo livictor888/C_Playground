08dd488caacffaf88015ad8c915c2ad37369d0bd
livictor888 <vli30@my.bcit.ca>
2021-10-13 18:01:54 -0700

    A2

--- comp2510/Assignment_2.c
+++ comp2510/Assignment_2.c
@@ -1,33 +1,48 @@
-// start with string
-// while keeping track of line length and character position
-// if it equals the first char, start the counting
-// if nth character doesn't match, reset the counting
-
 #include <stdio.h>
+#include <stdbool.h>
 
 #define ERROR_FILE_NOT_FOUND 1
 #define ERROR_TOO_MANY_ARGUMENTS 2
 #define ERROR_MISSING_ARGUMENTS 3
 
-void matchSearchToken(FILE *file, char *searchToken) {
-    int lineNumber;
-    char currentChar;
-    fscanf(file, "%c", currentChar);
-    if (currentChar == *searchToken) {
-        printf("character matches\n");
-    }
+void lookForFirstLetter(FILE *file, char *searchToken) {
+    int lineNumber = 1;
+    int charNumber = 1;
+    char currentChar = (char) fgetc(file);
+    while (currentChar != EOF) {
+        if (currentChar == '\n') { // case 1: encounter a new line
+            lineNumber++;
+            charNumber = 1;
+            currentChar = (char) fgetc(file);
+        } else if (currentChar == *searchToken) { // case 2: match the first character of the searchToken
+            char *pointer = searchToken;
+            while (currentChar == *pointer) { // match with the rest of the searchToken
+                pointer++;
+                currentChar = (char) fgetc(file);
+                if (*pointer == '\0') {
+                    printf("character is at line: %d, character: %d\n", lineNumber, charNumber);
+                    return;
+                }
+            }
+            charNumber++;
+        } else { // case 3 : character doesn't match
+            currentChar = (char) fgetc(file); // check the next character
+            charNumber++;
+        }
 
+    }
+    printf("Character not found.\n");
 }
 
-int processFile(char *fileName, char *searchToken) {
+bool processFile(char *fileName, char *searchToken) {
     FILE *file = fopen(fileName, "r");
     if (file == NULL) {
         printf("File not found.\n");
-        return ERROR_FILE_NOT_FOUND;
+        return false;
     }
-    matchSearchToken(file, searchToken);
+    lookForFirstLetter(file, searchToken);
     fclose(file);
-    return 0;
+    return true;
 }
 
 int main(int argc, char *argv[]) {
@@ -40,7 +55,10 @@ int main(int argc, char *argv[]) {
         printf("The arguments supplied are %s, %s\n", argv[1], argv[2]);
         char *fileName = argv[1];
         char *searchToken = argv[2];
-        processFile(fileName, searchToken);
+        bool succeeded = processFile(fileName, searchToken);
+        if (succeeded == false) {
+            return ERROR_FILE_NOT_FOUND;
+        }
     } else if (argc > 3) {
         printf("Too many arguments supplied.\n");
         return ERROR_TOO_MANY_ARGUMENTS;
