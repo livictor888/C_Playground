a9748c7802cf25358a2598fca0d05b47027c6cca
livictor888 <vli30@my.bcit.ca>
2021-11-07 22:58:09 -0800

    File handling

--- comp2510/Assignment_3.c
+++ comp2510/Assignment_3.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define EXPECTED_NUMBER_OF_ARGUMENTS 1
+#define INITIAL_SIZE 5
 
+#define INVALID_NUMBER_OF_ARGUMENTS_CODE 1
+#define FILE_NOT_FOUND_CODE 2
 
+void storeIntoArray();
 
-int main() {
+void processFile(char *fileName) {
+    FILE *file = fopen(fileName, "r");
+    if (file == NULL) {
+        perror("File not found.");
+        exit(FILE_NOT_FOUND_CODE);
+    }
+    // do stuff
+    fclose(file);
+}
+
+void validateNumberOfArguments(int argc) {
+    if (argc != EXPECTED_NUMBER_OF_ARGUMENTS + 1) {
+        perror("Need exactly 2 arguments");
+        exit(INVALID_NUMBER_OF_ARGUMENTS_CODE);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    printf("The program name is %s\n", argv[0]);
+    validateNumberOfArguments(argc);
+    processFile(argv[1]);
+
     return 0;
 }
