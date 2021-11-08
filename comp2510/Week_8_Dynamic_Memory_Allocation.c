f9ff60399805152ee83f4b035a8dabca5735ad16
livictor888 <vli30@my.bcit.ca>
2021-11-08 15:44:02 -0800

    Week 8 Practice

---
+++ comp2510/Week_8_Dynamic_Memory_Allocation.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INITIAL_SIZE 5
+
+int *resizeArrayIfNeeded(int *array, int usedLength, int *arraySize) {
+    if (usedLength <= *arraySize) {
+        return array;
+    }
+    printf("Oops, need to resize!\n");
+    *arraySize *= 2;
+    return (int *) realloc(array, *arraySize * sizeof(int));
+}
+
+int main(void) {
+    int length = 0, currentNumber = 0;
+    int size = INITIAL_SIZE;
+    int *array = (int *) malloc(size * sizeof(int));
+    scanf("%d", &currentNumber);
+    while (currentNumber > 0) {
+        length++;
+        array = resizeArrayIfNeeded(array, length, &size);
+        array[length - 1] = currentNumber;
+        scanf("%d", &currentNumber);
+    }
+    for (int i = 0; i < length; i++) {
+        printf("A[%d] = %d\n", i, array[i]);
+    }
+    free(array);
+    return 0;
+}
