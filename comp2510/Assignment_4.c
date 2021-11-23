767a7a7d8d5e162238becb683b2f44016e9b1e94
livictor888 <vli30@my.bcit.ca>
2021-11-22 23:26:12 -0800

    A4

---
+++ comp2510/Assignment_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INVALID_NUMBER_OF_ARGUMENTS_CODE 1
+#define FILE_NOT_FOUND_CODE 2
+#define FAILED_TO_ALLOCATE_MEMORY 3
+
+#define EXPECTED_NUMBER_OF_ARGUMENTS 1
+
+int main() {
+    return 0;
+}
