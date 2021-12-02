f63125746437413cd941eb0fa22fa137f2cd681e
Victor Li <vli30@my.bcit.ca>
2021-12-01 23:47:18 -0800

    A4

--- comp2510/Assignment_4.c
+++ comp2510/Assignment_4.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdbool.h>
 
 #define MEMORY_ALLOCATION_ERROR_CODE 1
@@ -8,8 +7,8 @@
 #define EMPTY_LIST_POP_ERROR_CODE 3
 
 struct Node {
-    bool isProcess;
-    char *pid;
+    bool isHole;
+    int pid;
     int base;
     int limit;
     struct Node *next;
@@ -17,24 +16,53 @@ struct Node {
 
 typedef struct Node* Link;
 
-Link createNode(bool isProcess, char *pid, int base, int limit) {
+Link createNode(bool isHole, int pid, int base, int limit) {
     Link link = (Link) malloc(sizeof(struct Node));
     if (link == NULL) {
         perror("Failed to allocate memory.");
         exit(MEMORY_ALLOCATION_ERROR_CODE);
     }
-    link->isProcess = isProcess;
-    strcpy(link->pid, pid);
+    link->isHole = isHole;
+    link->pid = pid;
     link->base = base;
     link->limit = limit;
     link->next = NULL;
     return link;
 }
 
-Link createNodeWithNextNode(bool isProcess, char *pid, int base, int limit, Link next) {
-    Link link = createNode(isProcess, pid, base, limit);
+Link createNodeWithNextNode(bool isHole, int pid, int base, int limit, Link next) {
+    Link link = createNode(isHole, pid, base, limit);
     link->next = next;
     return link;
+}
+
+Link getTail(Link head) {
+    if (!head) {
+        return NULL;
+    }
+
+    if (!head->next) {
+        return head;
+    }
+
+    return getTail(head->next);
+}
+
+void addLast(Link *head, bool isHole, int pid, int base, int limit) {
+    Link newTail = createNode(isHole, pid, base, limit);
+    if (!*head) {
+        *head = newTail;
+        return;
+    }
+    Link currentTail = getTail(*head);
+    currentTail->next = newTail;
+}
+
+void push(Link *head, bool isHole, int pid, int base, int limit) {
+    Link newHead = createNodeWithNextNode(isHole, pid, base, limit, *head);
+    //4->20->10
+    //5->4->20->10
+    *head = newHead;
 }
 
 void printLink(Link head, int index) {
@@ -42,8 +70,8 @@ void printLink(Link head, int index) {
         printf("\n");
         return;
     }
-    if (head->isProcess) {
-        printf("Node %d: %s, base = %d, limit = %d\n", index, head->pid, head->base, head->limit);
+    if (head->isHole) {
+        printf("Node %d: P%d, base = %d, limit = %d\n", index, head->pid, head->base, head->limit);
         printLink(head->next, index + 1);
     } else {
         printf("Node %d: H (Hole), base = %d, limit = %d\n", index, head->base, head->limit);
@@ -54,10 +82,47 @@ void printMemory(Link head) {
     printLink(head, 1);
 }
 
+void mergeFreeBlocks(Link head) {
+    /*
+     * traverse through the linked list
+     * if you encounter a hole and then the link is also a hole,
+     * delete the current hole
+     */
+    Link current = head;
+    Link lookAhead = head->next;
+    while (current != NULL && lookAhead != NULL) {
+        if (current->isHole && lookAhead->isHole) {
+            current->limit += lookAhead->limit;
+            current->next = lookAhead->next;
+            free(lookAhead);                 // if I merge, move the stats of the lookahead into the current,
+            // do I need to free the node afterwards?
+            lookAhead = current->next;
+            //Link lookahead = current->next;
+        } else {
+            current = lookAhead;
+            lookAhead = current->next;
+        }
+    }
+}
+
 int main() {
     Link head = NULL;
-    head = createNode(true, "hello", 0, 5);
+//    head = createNode(true, 2, 0, 5);
+//    push(&head, false, 0, 11, 4);
+//    push(&head, false, 0, 7, 4);
+//    push(&head, true, 1, 6, 1);
 
+    printf("\ntwo hole test\n");
+    head = NULL;
+    push(&head, true, 0, 10, 12);
+    push(&head, true, 0, 0, 10);
     printMemory(head);
+    printf("\n");
+    mergeFreeBlocks(head);
+    printMemory(head);
+
+//    printMemory(head);
+//    mergeFreeBlocks(head);
+//    printMemory(head);
     return 0;
 }
